a2e56db8bc359fd09b7128f0e90edda0dc265166
Mikhail Bortnyk <vessimir@gmail.com>
2016-09-12 21:49:32 +0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in di_cluster-redis.gemspec
+gemspec
---
+++ lib/di_cluster/registry/redis.rb
@@ -0,0 +1,77 @@
+require 'redis'
+require 'redis-namespace'
+require 'msgpack'
+
+module DiCluster
+  module Registry
+    class Redis
+      def initialize(options = {})
+        options = options.each_with_object({}) { |(k,v), obj| obj[k.to_sym] = v }
+        environment = DiCluster.environment
+        @redis = ::Redis::Namespace.new "di_cluster_#{environment}", redis: ::Redis.new(options)
+      end
+
+      def nodes
+        nodes = keys('nodes')
+        nodes.each do |node|
+          unregister(node) if Time.at(node(node)['ttl']) < Time.now
+        end
+        nodes
+      end
+
+      def nodes_with_role(role_name)
+        Hash[all('nodes').map { |k, v| [k, MessagePack.unpack(v)] }].select { |k,v| v["roles"].include?(role_name) }
+      end
+
+      def node(node_name)
+        get('nodes', node_name)
+      end
+      alias :[] :node
+
+      def register(node_name, options)
+        set('nodes', node_name, options)
+      end
+
+      def update(node_name, options)
+        value = get('nodes', node_name)
+        set('nodes', node_name, value.merge(options))
+      end
+
+      def unregister(node_name)
+        remove('nodes', node_name)
+      end
+
+      def clear_nodes
+        delete('nodes')
+      end
+
+      private
+
+      def all(table)
+        @redis.hgetall(table)
+      end
+
+      def keys(table)
+        @redis.hkeys(table)
+      end
+
+      def set(table, key, value)
+        @redis.hset table, key.to_s, value.to_msgpack
+      end
+
+      def get(table, key)
+        value = @redis.hget table, key.to_s
+        value = MessagePack.unpack(value) if value
+        value
+      end
+
+      def remove(table, key)
+        @redis.hdel table, key
+      end
+
+      def delete(table)
+        @redis.del table
+      end
+    end
+  end
+end
